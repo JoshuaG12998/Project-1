24b11957e5e18106432a28ba07d58562cdab15d0
Joshua Garcia <j.garcia12998@gmail.com>
2014-09-19 00:38:31 -0700

    lul commit

--- js/main.js
+++ js/main.js
@@ -45,20 +45,3 @@ $(window).scroll(function(){
 });
 
 
-var photoTh = $('.picture'),
-    overlay = $('.overlay');
-
-photoTh.on('click', function() {    
-    var dataPhoto = $(this).attr('src'),
-        dataTitle = $(this).data('title');
-  
-    overlay.show();
-    
-    $('.picture-big').attr("src", dataPhoto);
-    $('.photo-title').text(dataTitle);
-  }
-);
-
-overlay.on('click', function() {
-  $(this).hide();
-});

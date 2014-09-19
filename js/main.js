681e2c2bc99036733b0c11cc112ff43e40fcc655
Joshua Garcia <j.garcia12998@gmail.com>
2014-09-18 23:52:06 -0700

    More Commits!

--- js/main.js
+++ js/main.js
@@ -43,3 +43,22 @@ $(window).scroll(function(){
          .end().filter("[href=#"+id+"]").parent().addClass("active");
    }                   
 });
+
+
+var photoTh = $('.picture'),
+    overlay = $('.overlay');
+
+photoTh.on('click', function() {    
+    var dataPhoto = $(this).attr('src'),
+        dataTitle = $(this).data('title');
+  
+    overlay.show();
+    
+    $('.picture-big').attr("src", dataPhoto);
+    $('.photo-title').text(dataTitle);
+  }
+);
+
+overlay.on('click', function() {
+  $(this).hide();
+});
